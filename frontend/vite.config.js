10ff18e5551c0b718e8932213cbd399370b314b2
PremRathan2003 <premrathanaddanki7736@gmail.com>
2025-12-02 18:20:43 +0000

    vercel changes  commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Prescripto/',   // <-- IMPORTANT
-  plugins: [react()]
+  plugins: [react()],
+  base: '/Prescripto/' // 👈 your repo name
 })
